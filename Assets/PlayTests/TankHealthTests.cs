e2b2e3a8c4d97ed2b177e2a0abe69f866f803e0f
Zeptosec <armandasmatkevicius@gmail.com>
2023-11-05 00:27:42 +0200

    Added 2 TankManager tests and 3 TankHealthTests

--- Assets/PlayTests/TankHealthTests.cs
+++ Assets/PlayTests/TankHealthTests.cs
@@ -4,7 +4,7 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 
-public class NewTestScript
+public class TankHealthTests
 {
     // A Test behaves as an ordinary method
     [Test]
@@ -17,13 +17,34 @@ public class NewTestScript
         Assert.AreEqual(health.m_CurrentHealth, 90f);
     }
 
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
-    [UnityTest]
-    public IEnumerator NewTestScriptWithEnumeratorPasses()
+    [Test]
+    public void TankHealthSpawnWith100()
     {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
-        yield return null;
+        var tankPrefab = Resources.Load<GameObject>("Prefabs/Tank");
+        var tankObject = GameObject.Instantiate(tankPrefab);
+        var health = tankObject.GetComponent<TankHealth>();
+        // Use the Assert class to test conditions
+        Assert.AreEqual(100f, health.m_CurrentHealth);
     }
+
+    [Test]
+    public void TankHealthTankDied()
+    {
+        var tankPrefab = Resources.Load<GameObject>("Prefabs/Tank");
+        var tankObject = GameObject.Instantiate(tankPrefab);
+        var health = tankObject.GetComponent<TankHealth>();
+        health.TakeDamage(110f);
+        // Use the Assert class to test conditions
+        Assert.AreEqual(true, health.m_Dead);
+    }
+
+    //// A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+    //// `yield return null;` to skip a frame.
+    //[UnityTest]
+    //public IEnumerator NewTestScriptWithEnumeratorPasses()
+    //{
+    //    // Use the Assert class to test conditions.
+    //    // Use yield to skip a frame.
+    //    yield return null;
+    //}
 }
